df3b9fe0ad039527d8e6d3b422e8d99d5f745100
Devota Aabel <daabel@mozilla.com>
2025-07-28 07:03:23 +0000

    Bug 1974315 - Part 1: Cleanup homepage section settings by moving to the same place.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D255219
    
    Differential Revision: https://phabricator.services.mozilla.com/D257059

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/store/HomepageState.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/store/HomepageState.kt
@@ -27,8 +27,6 @@ import org.mozilla.fenix.home.recentsyncedtabs.RecentSyncedTabState
 import org.mozilla.fenix.home.recenttabs.RecentTab
 import org.mozilla.fenix.home.recentvisits.RecentlyVisitedItem
 import org.mozilla.fenix.home.topsites.TopSiteColors
-import org.mozilla.fenix.nimbus.FxNimbus
-import org.mozilla.fenix.nimbus.HomeScreenSection
 import org.mozilla.fenix.search.SearchDialogFragment
 import org.mozilla.fenix.utils.Settings
 
@@ -174,7 +172,7 @@ internal sealed class HomepageState {
                         showTopSites = settings.showTopSitesFeature && topSites.isNotEmpty(),
                         showRecentTabs = shouldShowRecentTabs(settings),
                         showBookmarks = settings.showBookmarksHomeFeature && bookmarks.isNotEmpty(),
-                        showRecentSyncedTab = shouldShowRecentSyncedTabs() && showSyncedTab,
+                        showRecentSyncedTab = shouldShowRecentSyncedTabs() && settings.showSyncedTabs,
                         showRecentlyVisited = settings.historyMetadataUIFeature && recentHistory.isNotEmpty(),
                         showPocketStories = settings.showPocketRecommendationsFeature &&
                             recommendationState.pocketStories.isNotEmpty() && firstFrameDrawn,
@@ -195,9 +193,6 @@ internal sealed class HomepageState {
         }
     }
 }
-
-private val showSyncedTab: Boolean
-    get() = FxNimbus.features.homescreen.value().sectionsEnabled[HomeScreenSection.SYNCED_TABS] == true
 
 @Composable
 private fun getBottomSpace(): Dp {
