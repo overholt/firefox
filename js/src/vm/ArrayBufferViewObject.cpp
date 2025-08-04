e75a2ff1c1ea0d79b5546ffedc8b34d755736281
Matthew Gaudet <mgaudet@mozilla.com>
2025-08-04 12:54:30 +0000

    Bug 1970154 - Streams Realm Handling Improvements (esr115) r=jonco,saschanaz a=diannaS DONTBUILD
    Make TransferArrayBuffer cautious, and remove overzealous assert in JS_GetArrayBufferViewBuffer
    
    Differential Revision: https://phabricator.services.mozilla.com/D259300

--- js/src/vm/ArrayBufferViewObject.cpp
+++ js/src/vm/ArrayBufferViewObject.cpp
@@ -204,7 +204,6 @@ JS_PUBLIC_API JSObject* JS_GetArrayBufferViewBuffer(JSContext* cx,
                                                     bool* isSharedMemory) {
   AssertHeapIsIdle();
   CHECK_THREAD(cx);
-  cx->check(obj);
 
   Rooted<ArrayBufferViewObject*> unwrappedView(
       cx, obj->maybeUnwrapAs<ArrayBufferViewObject>());
