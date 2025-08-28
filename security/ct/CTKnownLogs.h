bd93330ff9f3104ef965cc2b0f4115ec6472affd
ffxbld <ffxbld@mozilla.com>
2025-08-28 13:56:14 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP remote-settings tld-suffixes ct-logs -  a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D262957

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1762169198000000);
+static const PRTime kCTExpirationTime = INT64_C(1762437079000000);
 
 namespace mozilla::ct {
 
