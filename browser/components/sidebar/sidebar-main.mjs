680f49c103efdb5664820bde6dcc201f2c60bb87
Sarah Clements <sclements@mozilla.com>
2025-09-25 17:17:04 +0000

    Bug 1988996 - Only show promo card when checkForPinnedTabsComplete is truthy  a=RyanVM DONTBUILD
    * Turn SidebarManager into a class that extends from EventTarget so as to dispatch a custom event
    
    Original Revision: https://phabricator.services.mozilla.com/D265278
    
    Differential Revision: https://phabricator.services.mozilla.com/D265947

--- browser/components/sidebar/sidebar-main.mjs
+++ browser/components/sidebar/sidebar-main.mjs
@@ -152,6 +152,9 @@ export default class SidebarMain extends MozLitElement {
 
     this._toolsIntersectionObserver?.disconnect();
     this._toolsResizeObserver?.disconnect();
+    this.ownerDocument
+      .getElementById("drag-to-pin-promo-card")
+      ?.disconnectedCallback();
   }
 
   get isToolsDragging() {
