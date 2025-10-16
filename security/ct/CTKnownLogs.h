858c610e9313f0722352ec39c638ec71c4d11fc0
ffxbld <ffxbld@mozilla.com>
2025-10-16 14:09:28 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings tld-suffixes ct-logs - r=release-managers,diannaS
    Differential Revision: https://phabricator.services.mozilla.com/D268853

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1766406081000000);
+static const PRTime kCTExpirationTime = INT64_C(1766660418000000);
 
 namespace mozilla::ct {
 
