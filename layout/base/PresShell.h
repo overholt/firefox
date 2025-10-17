998d878da24d0b2889cfff28337b20d9e8b74796
Emilio Cobos Álvarez <emilio@crisal.io>
2025-10-17 06:33:35 +0000

    Bug 1994893 - Remove VerifyIncrementalReflow and co. r=layout-reviewers,TYLin
    I bet nobody has used this since forever. Just running my local central build
    (without the previous patch) with GECKO_VERIFY_REFLOW_FLAGS=verify stack
    overflows, and it probably has at least for years.
    
    Keeping this code around seems unnecessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D268977

--- layout/base/PresShell.h
+++ layout/base/PresShell.h
@@ -740,18 +740,6 @@ class PresShell final : public nsStubDocumentObserver,
    */
   MOZ_CAN_RUN_SCRIPT void ReconstructFrames();
 
-  /**
-   * See if reflow verification is enabled. To enable reflow verification add
-   * "verifyreflow:1" to your MOZ_LOG environment variable (any non-zero
-   * debug level will work). Or, call SetVerifyReflowEnable with true.
-   */
-  static bool GetVerifyReflowEnable();
-
-  /**
-   * Set the verify-reflow enable flag.
-   */
-  static void SetVerifyReflowEnable(bool aEnabled);
-
   nsIFrame* GetAbsoluteContainingBlock(nsIFrame* aFrame);
 
   // https://drafts.csswg.org/css-anchor-position-1/#target
@@ -3182,11 +3170,7 @@ class PresShell final : public nsStubDocumentObserver,
   VisibleFrames mApproximatelyVisibleFrames;
 
 #ifdef DEBUG
-  MOZ_CAN_RUN_SCRIPT_BOUNDARY bool VerifyIncrementalReflow();
-  MOZ_CAN_RUN_SCRIPT_BOUNDARY void DoVerifyReflow();
   void VerifyHasDirtyRootAncestor(nsIFrame* aFrame);
-
-  bool mInVerifyReflow = false;
   // The reflow root under which we're currently reflowing.  Null when
   // not in reflow.
   nsIFrame* mCurrentReflowRoot = nullptr;
