d7622487ef3148762fc3fce4b32c91b7a4a5795c
agoloman <agoloman@mozilla.com>
2025-10-17 21:36:26 +0300

    Revert "Bug 1994964 - Simplify nsContentUtils::WidgetForDocument. r=tnikkel,layout-reviewers" for causing multiple failures.
    This reverts commit 3039907ce143833ffefdf300ad0bc5560f9c418c.

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -7798,8 +7798,19 @@ nsIWidget* nsContentUtils::WidgetForDocument(const Document* aDocument) {
   if (!presShell) {
     return nullptr;
   }
-  nsIFrame* rootFrame = presShell->GetRootFrame();
-  return rootFrame ? rootFrame->GetNearestWidget() : nullptr;
+  nsViewManager* vm = presShell->GetViewManager();
+  if (!vm) {
+    return nullptr;
+  }
+  nsView* rootView = vm->GetRootView();
+  if (!rootView) {
+    return nullptr;
+  }
+  nsView* displayRoot = nsViewManager::GetDisplayRootFor(rootView);
+  if (!displayRoot) {
+    return nullptr;
+  }
+  return displayRoot->GetNearestWidget(nullptr);
 }
 
 nsIWidget* nsContentUtils::WidgetForContent(const nsIContent* aContent) {
