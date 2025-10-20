d42d760c2d8402a4e6b4939e780693707fc7fce7
Hubert Boma Manilla <hmanilla@mozilla.com>
2025-10-20 13:42:25 +0000

    Bug 1994114 - [devtools] Show only one inline preview when binding which are accesible to multiple scopes r=devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D268747

--- devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-inline-preview.js
@@ -158,6 +158,14 @@ add_task(async function testInlinePreviews() {
     ],
   });
 
+  await invokeFunctionAndAssertInlinePreview({
+    dbg,
+    fnName: "innerBlockHoistedFuncDecl",
+    expectedInlinePreviews: [
+      { previews: [{ identifier: "foo:", value: "function foo()" }], line: 90 },
+    ],
+  });
+
   // Check inline previews for values within a module script
   await invokeFunctionAndAssertInlinePreview({
     dbg,
