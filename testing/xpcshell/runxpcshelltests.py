89fb98b5ec4ee9f5276f5369ab4efa5c01f864f1
Florian Quèze <florian@queze.net>
2025-10-21 20:35:27 +0000

    Bug 1995593 - don't upload crash dumps from self tests, r=jmaher.
    Differential Revision: https://phabricator.services.mozilla.com/D269435

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -366,8 +366,16 @@ class XPCShellTestThread(Thread):
         On a remote system, this is more complex and we need to overload this function.
         """
         quiet = self.crashAsPass or self.retry
+        # For selftests, set dump_save_path to prevent crash dumps from being saved
+        # (they intentionally crash and the dumps aren't useful artifacts)
+        dump_save_path = "" if self.selfTest else None
         return mozcrash.log_crashes(
-            self.log, dump_directory, symbols_path, test=test_name, quiet=quiet
+            self.log,
+            dump_directory,
+            symbols_path,
+            test=test_name,
+            quiet=quiet,
+            dump_save_path=dump_save_path,
         )
 
     def logCommand(self, name, completeCmd, testdir):
