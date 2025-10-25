5faf40439da7e5f4005f4661ebdd78075276b1ab
Thomas Wisniewski <twisniewski@mozilla.com>
2025-10-25 01:01:15 +0000

    Bug 1996081 - Add the ability to enable/disable individual webcompat interventions with about:config prefs; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D269880

--- browser/extensions/webcompat/tests/browser/browser_aboutcompat.js
+++ browser/extensions/webcompat/tests/browser/browser_aboutcompat.js
@@ -1,6 +1,9 @@
 "use strict";
 
 add_task(async function test_about_compat_loads_properly() {
+  // wait for all interventions to load before testing (can be quite slow on tsan builds).
+  await WebCompatExtension.noOngoingInterventionChanges();
+
   const tab = await BrowserTestUtils.openNewForegroundTab({
     gBrowser,
     opening: "about:compat",
