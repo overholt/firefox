ea0343adb07dec6d6af6068cddde8041bfe846db
Serban Stanca <sstanca@mozilla.com>
2025-10-28 18:51:46 +0200

    Revert "Bug 1949036 - [devtools] Fix browser_screenshot_button.js intermittent failures. r=devtools-reviewers,jdescottes" for causing conflicts trying to revert Bug 1979518.
    This reverts commit e37b703b019c9c48ddbd2da17e255283f4548069.

--- devtools/client/responsive/test/browser/browser_screenshot_button.js
+++ devtools/client/responsive/test/browser/browser_screenshot_button.js
@@ -19,17 +19,15 @@ for (const URL of [TEST_URL, TEST_URL2]) {
     const { toolWindow } = ui;
     const { document } = toolWindow;
 
-    const whenScreenshotSucceeded = waitUntilDownload();
-
     info("Click the screenshot button");
     const screenshotButton = document.getElementById("screenshot-button");
     screenshotButton.click();
 
+    const whenScreenshotSucceeded = waitUntilDownload();
+
     const filePath = await whenScreenshotSucceeded;
     const image = new Image();
-    // Bug 1949036: we run this test twice and the 2 screenshot files may have
-    // the same filename, so we need to add a cache buster.
-    image.src = PathUtils.toFileURI(filePath) + `?nocache=${Date.now()}`;
+    image.src = PathUtils.toFileURI(filePath);
 
     await once(image, "load");
 
